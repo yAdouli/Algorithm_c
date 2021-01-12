9d56297e4a189de22ef901f245c9f282dc6b087b
yAdouli <74469773+yAdouli@users.noreply.github.com>
2021-01-12 16:48:13 +0100

    Step 1 #1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Step 1: 
+
+## _*#1*_ : 
+
+```diff
+Ecrivez un programme en C qui va te permettre d'afficher vos informations personnelles : Nom, prenom , Age , Sexe et numero de telephone 
+Les données sont saisies à partir du clavier
+```
---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    // Instantiation of variables
+    char firstName[16];
+    char lastName[10];
+    int age = 0;
+    char sex[10];
+    int numberPhone = 0;
+
+    // Questioning phase
+    printf("Please, enter your name.\n");
+    scanf("%s", &firstName);
+    printf("Enter your lastName.\n");
+    scanf("%s" , &lastName);
+    printf("Enter your age.\n");
+    scanf("%d", &age);
+    printf("Male or Female.\n");
+    scanf("%s" , &sex);
+    printf("Enter your numberPhone.\n");
+    scanf("%d" , &numberPhone);
+
+    // Display results
+    printf("Actually, your name is %s,", firstName);
+    printf(" and your lastName is %s," , lastName);
+    printf(" your age is %d,", age);
+    printf(" you're a %s,", sex);
+    printf(" at last your number is 0%d.", numberPhone);
+
+    // End
+    return 0;
+}
