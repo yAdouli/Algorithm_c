74c0df8376805c378f1c925f21080cd001288b3e
yAdouli <74469773+yAdouli@users.noreply.github.com>
2021-01-13 08:51:06 +0100

    add ('#4')

---
+++ Step 1/#4/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float nombre, somme, x, moyenne;
+
+int main() 
+{
+    nombre = 0;
+    somme = 0;
+
+    printf("Entrez un entier positif: ");
+    scanf("%f", &x);
+
+    while (x >= 0) 
+    {
+        nombre++;
+        somme += x;
+
+       printf("Entrez un entier positif: ");
+        scanf("%f", &x);
+    }
+
+    printf("la moyenne est : %f\n", somme / nombre);
+
+    return 0;
+}
